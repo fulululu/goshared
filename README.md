6ec1d5da64d04dfa836660653af976ec894b7439
Fulululu <ygfulululu@gmail.com>
2021-08-17 09:57:00 +0800

    add new version of mysql client.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Introduction
-This repo is my golang package collections, codes here maybe be written by myself or copied from other places.
+This repo is my golang code collections, codes here maybe be written by myself or copied from other places. Some conventions are listed below:
+<br>
+- The codes is classified by file name;
+- The codes that not rely on third-party libraries is placed in the root directory;
+- The codes that rely on third-party libraries is placed in the `nonbuiltin` directory;
+
---
+++ nonbuiltin/mysql.go
@@ -0,0 +1,55 @@
+// Package nonbuiltin contains codes that rely on third-party libraries.
+package nonbuiltin
+
+import (
+	"database/sql"
+	"fmt"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type RawConfig struct {
+	IP                string `envconfig:"MYSQL_IP"`
+	Port              string `envconfig:"MYSQL_PORT,default=3306"`
+	TLP               string `envconfig:"MYSQL_TLP,optional"`
+	User              string `envconfig:"MYSQL_USER"`
+	Password          string `envconfig:"MYSQL_PASSWORD"`
+	Database          string `envconfig:"MYSQL_DATABASE"`
+	ConnMaxLifeMinute int    `envconfig:"MYSQL_CONN_MAX_LIFE_MINUTE,default=3"`
+	MaxOpenConns      int    `envconfig:"MYSQL_MAX_OPEN_CONNS,default=10"`
+	MaxIdleConns      int    `envconfig:"MYSQL_MAX_IDLE_CONNS,default=10"`
+}
+
+type ORMConfig struct {
+	GORMDialector mysql.Config
+	GORM          gorm.Config
+}
+
+func NewMySQLClient(rawCfg RawConfig, ormCfg ORMConfig) (*gorm.DB, error) {
+	// Raw layer
+	dsn := fmt.Sprintf("%s:%s@%s(%s:%s)/%s?parseTime=True&loc=Local", rawCfg.User, rawCfg.Password, "tcp", rawCfg.IP, rawCfg.Port, rawCfg.Database)
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	db.SetConnMaxLifetime(time.Minute * time.Duration(rawCfg.ConnMaxLifeMinute))
+	db.SetMaxOpenConns(rawCfg.MaxOpenConns)
+	db.SetMaxIdleConns(rawCfg.MaxIdleConns)
+
+	// ORM layer
+	ormCfg.GORMDialector.Conn = db
+	gormDialector := mysql.New(ormCfg.GORMDialector)
+	gormCfg := ormCfg.GORM
+	gormDB, e := gorm.Open(gormDialector, &gormCfg)
+	if e != nil {
+		return nil, e
+	}
+
+	return gormDB, nil
+}
